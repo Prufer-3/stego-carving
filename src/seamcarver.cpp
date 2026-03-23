f7699c8855a4b70a6ccfc9ee6414075769c4858e
Prufer-3 <>
2026-03-23 19:38:15 +1100

    Tests for multi seam removal (both directions)

--- src/seamcarver.cpp
+++ src/seamcarver.cpp
@@ -55,6 +55,8 @@ void SeamCarver::transpose() {
     std::swap(width, height);
 }
 
+// TODO: Consider removing this. Energy matrix should not be exposed.
+// Will need to rewrite/delete some tests.
 cv::Mat SeamCarver::energy() const {
     if (transposed) {
         return energy_matrix.t();
